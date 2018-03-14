3bd78e2b1dab72e36dfd9f5ef21c14e606810694
devac1103 <hdev0301@gmail.com>
2018-03-14 09:43:40 +0800

    Modified ChatMenu screen to have buttons

--- src/Screens/ChatMenu.js
+++ src/Screens/ChatMenu.js
@@ -2,16 +2,50 @@
 import React, { Component } from 'react'
 import { View, Text, Dimensions, StyleSheet } from 'react-native'
 
+//Assets
+import ModalMenuButton from '../Components/ModalMenuButton'
+import HomeButton from '../Components/HomeButton'
+
 const { height,width } = Dimensions.get('window')
 
+onChat = () => {
+    console.log('chat clicked')
+}
+
+onGroupChat = () => {
+    console.log('group chat clicked')
+}
+
+onCancel = () => {
+    console.log('cancel clicked')
+}
+
 const ChatMenu = () => {
-    return(
-        <View style={styles.mainContainer}></View>
+    return (
+        <View style={styles.mainContainer}>
+            <ModalMenuButton
+                label="Chat"
+                bgColor="#F7A553"
+                onPress={() => onChat()}
+            />
+            <ModalMenuButton
+                label="Group Chat"
+                bgColor="#F7A553"
+                onPress={() => onGroupChat()}
+            />
+            <ModalMenuButton
+                label="Cancel"
+                bgColor="white"
+                onPress={() => onCancel()}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
         width: width,
         height: height,
         backgroundColor: 'rgb(0,131,105)',
