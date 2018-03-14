ba92a100642d377ab851b6a6c1a9840eefbc1377
devac1103 <hdev0301@gmail.com>
2018-03-14 10:46:11 +0800

    Modified ChatMenu screen to navigate to ChatScreen

--- src/Screens/ChatMenu.js
+++ src/Screens/ChatMenu.js
@@ -4,39 +4,26 @@ import { View, Text, Dimensions, StyleSheet } from 'react-native'
 
 //Assets
 import ModalMenuButton from '../Components/ModalMenuButton'
-import HomeButton from '../Components/HomeButton'
 
 const { height,width } = Dimensions.get('window')
 
-onChat = () => {
-    console.log('chat clicked')
-}
-
-onGroupChat = () => {
-    console.log('group chat clicked')
-}
-
-onCancel = () => {
-    console.log('cancel clicked')
-}
-
-const ChatMenu = () => {
+const ChatMenu = ({ onChat, onGroupChat, onCancel }) => {
     return (
         <View style={styles.mainContainer}>
             <ModalMenuButton
                 label="Chat"
                 bgColor="#F7A553"
-                onPress={() => onChat()}
+                onPress={onChat}
             />
             <ModalMenuButton
                 label="Group Chat"
                 bgColor="#F7A553"
-                onPress={() => onGroupChat()}
+                onPress={onGroupChat}
             />
             <ModalMenuButton
                 label="Cancel"
                 bgColor="white"
-                onPress={() => onCancel()}
+                onPress={onCancel}
             />
         </View>
     )
