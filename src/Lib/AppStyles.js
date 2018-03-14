3e27ba7f5f4765fe63aae15eb262d5fc4fbc3697
devac1103 <hdev0301@gmail.com>
2018-03-15 04:03:35 +0800

    Added AppStyle & Constants for global app styles and variables

---
+++ src/Lib/AppStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+const AppStyles = StyleSheet.create({
+    center: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default AppStyles
---
+++ src/Lib/Constants.js
@@ -0,0 +1,7 @@
+const Colors = {
+    primaryBgColor: 'rgb(0 , 131, 105)',
+    secondaryBgColor: 'rgb(0, 143, 120)',
+    lightGreen: 'rgb(58, 152, 132)'
+}
+
+export default Colors
