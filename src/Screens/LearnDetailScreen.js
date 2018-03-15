23b9e8bf75965b15a85792854deb4d4516d07065
devac1103 <hdev0301@gmail.com>
2018-03-16 01:47:52 +0800

    Added LearnDetailScreen skeleton

---
+++ src/Screens/LearnDetailScreen.js
@@ -0,0 +1,13 @@
+// React
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class LearnDetailScreen extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: 'red' }}>
+                Detail
+            </View>
+        )
+    }
+}
