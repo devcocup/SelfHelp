2f43cf4f8ef1763ed2fcf63b928b1d69967728ce
devac1103 <hdev0301@gmail.com>
2018-03-16 04:26:06 +0800

    Modified constants for color and LearnDetailScreen labels

--- src/Lib/Constants.js
+++ src/Lib/Constants.js
@@ -1,7 +1,8 @@
 const Colors = {
     primaryBgColor: 'rgb(0 , 131, 105)',
     secondaryBgColor: 'rgb(0, 143, 120)',
-    lightGreen: 'rgb(58, 152, 132)'
+    lightGreen: 'rgb(58, 152, 132)',
+    orange: '#F7A553'
 }
 
 const FontSizes = {
@@ -16,22 +17,26 @@ const LearnLabels = [
             {
                 icon: require('../Assets/Images/building_hope.png'),
                 subTopic: 'Building Hope & Resiliency',
-                description: 'A self guided education program that helps you begin to recover and heal.'
+                description: 'A self guided education program that helps you begin to recover and heal.',
+                subContent: []
             },
             {
                 icon: require('../Assets/Images/what_to_do.png'),
                 subTopic: 'What to do',
-                description: 'Learn what to do if you or someone you know has been sexually assaulted.'
+                description: 'Learn what to do if you or someone you know has been sexually assaulted.',
+                subContent: []
             },
             {
                 icon: require('../Assets/Images/effects_of_sexual_assault.png'),
                 subTopic: 'Effects of Sexual Assault',
-                description: 'Here are some common effects survivors of sexual assault may face.'
+                description: 'Here are some common effects survivors of sexual assault may face.',
+                subContent: [ 'Depression', 'Eating Disorders', 'PTSD', 'Self Harm', 'Sleep Disturbances', 'Substance Abuse', 'Suicide']
             },
             {
                 icon: require('../Assets/Images/reduce_your_risk.png'),
                 subTopic: 'Reduce Your Risk',
-                description: 'Tips on how to reduce your risk of being sexually assaulted.'
+                description: 'Tips on how to reduce your risk of being sexually assaulted.',
+                subContent: []
             }
         ]
     },
@@ -41,12 +46,14 @@ const LearnLabels = [
             {
                 icon: require('../Assets/Images/tsm_overview.png'),
                 subTopic: 'TSM Overview',
-                description: 'Sexual assault Information for Transitioning Service Members.'
+                description: 'Sexual assault Information for Transitioning Service Members.',
+                subContent: []
             },
             {
                 icon: require('../Assets/Images/benefits_and_services.png'),
                 subTopic: 'Benefits and Services',
-                description: 'Services & Benefits for survivors of sexual assault who are TSMs'
+                description: 'Services & Benefits for survivors of sexual assault who are TSMs',
+                subContent: []
             }
         ]
     }
