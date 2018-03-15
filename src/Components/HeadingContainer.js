80427c7473e145b42eb55bfc3d6dee7b5bdc0b9a
devac1103 <hdev0301@gmail.com>
2018-03-15 11:21:11 +0800

    Added HeadingContainer for global purpose to be used dynamically in screens according to screen type

---
+++ src/Components/HeadingContainer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+const { height, width } = Dimensions.get("window")
+
+const HeadingContainer = ({ headingImage, headingText }) => {
+    return (
+        <View style={[styles.headingContainer, AppStyles.hCenter]}>
+            <View style={styles.iconArea}>
+                <Image source={headingImage} style={styles.imgStyle} />
+            </View>
+            <View style={styles.textArea}>
+                <Text style={styles.textStyle}>{headingText}</Text>
+            </View>
+        </View>
+    )
+}
+
+var styles = StyleSheet.create({
+    headingContainer: {
+        flexDirection: 'row',
+        height: height / 4,
+        width,
+        backgroundColor: "white",
+    },
+
+    iconArea: {
+        flex: .4,
+        alignItems: 'flex-end'
+    },
+
+    imgStyle: {
+        width: 90,
+        height: 60
+    },
+
+    textArea: {
+        flex: .6
+    },
+
+    textStyle: {
+        color: 'black',
+        fontSize: Constants.FontSizes.headingFS,
+        fontWeight: '900',
+        fontFamily: 'Arial-BoldMT',
+        fontWeight: 'bold',
+        marginLeft: 5
+    }
+})
+
+export default HeadingContainer
