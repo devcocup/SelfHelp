ed86f79260fa3bcd1333be9d10279a9cf26150a2
devac1103 <hdev0301@gmail.com>
2018-03-16 04:27:10 +0800

    Added SubTopicButton to be used in LearnDetailScreen

---
+++ src/Components/SubTopicButton.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+const { height, width } = Dimensions.get('window')
+
+const SubTopicButton = ({ text }) => {
+    return (
+        <TouchableOpacity>
+            <View style={[styles.button, AppStyles.center]}>
+                <Text style={styles.buttonText}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        height: height / 14,
+        marginTop: 15,
+        width: width - 40,
+        backgroundColor: Constants.Colors.lightGreen,
+        borderRadius: 4
+    },
+
+    buttonText: {
+        color: 'white',
+        fontWeight: "600",
+        fontSize: 18
+    }
+})
+
+export default SubTopicButton
