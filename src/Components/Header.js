b5af03b966431b0a094adf7a0780baeaa9fa922d
devac1103 <hdev0301@gmail.com>
2018-03-15 10:05:28 +0800

    Modified Header component with updated icon style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+
+// Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
 import Constants from '../Lib/Constants'
 
 const { height,width } = Dimensions.get('window')
-const MenuIcon = require('../Assets/Images/menu.png')
+const MenuIcon = require('../Assets/Images/nav_menu.png')
 
 const Header = ({ type }) => {
     const headerText = (type === 'Home') ? 'Safe Helpline' : 'Back'
@@ -15,10 +17,16 @@ const Header = ({ type }) => {
                 <Text style={styles.textStyle}>{headerText}</Text>
             </View>
             <View style={[styles.checkArea, AppStyles.center]}>
-                <Image source={MenuIcon} />
+                <Image
+                    source={MenuIcon}
+                    style={styles.checkIcon}
+                />
             </View>
             <View style={[styles.menuArea, AppStyles.center]}>
-                <Image source={MenuIcon} />
+                <Image
+                    source={MenuIcon}
+                    style={styles.menuIcon}
+                />
             </View>
         </View>
     )
@@ -50,10 +58,20 @@ const styles = StyleSheet.create({
         borderLeftColor: Constants.Colors.lightGreen
     },
 
+    checkIcon: {
+        width: 20,
+        height: 20
+    },
+
     menuArea: {
         flex: .17,
         borderLeftWidth: 2,
         borderLeftColor: Constants.Colors.lightGreen
+    },
+
+    menuIcon: {
+        width: 30,
+        height: 20
     }
 
 })
