237d0486cd45cd3e45717d6dd84bf1dd9b93d4a4
devac1103 <hdev0301@gmail.com>
2018-03-16 10:01:14 +0800

    Modified SelfCareScreen with modular structure with customized components

--- src/Screens/SelfCareScreen.js
+++ src/Screens/SelfCareScreen.js
@@ -1,6 +1,6 @@
-//React
+// React
 import React, { Component } from 'react'
-import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView, StyleSheet, Dimensions } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -11,7 +11,55 @@ import Header from '../Components/Header'
 import HeadingContainer from '../Components/HeadingContainer'
 
 const SelfCareIcon = require('../Assets/Images/self_care_orange.png')
+const NewPlanIcon = require('../Assets/Images/create_a_plan.png')
+const BrowseIcon = require('../Assets/Images/browse_past_plans.png')
+
 const { height, width } = Dimensions.get('window')
+const { Colors, FontSizes } = Constants
+
+const CardWithImage = ({ cardImage, text }) => {
+    return (
+        <TouchableOpacity>
+            <View style={[styles.buttonWithImage, AppStyles.center]}>
+                <View style={[styles.cardImageArea, AppStyles.center]}>
+                    <Image source={cardImage} style={styles.cardImageStyle} />
+                </View>
+                <View style={styles.cardTextArea}>
+                    <Text style={styles.buttonText}>{text}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const CardWithoutImage = ({ text }) => {
+    return (
+        <TouchableOpacity>
+            <View style={[styles.buttonWithoutImage, AppStyles.center]}>
+                <Text style={styles.buttonText}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const CardContainer = () => {
+    return (
+        <View style={[styles.cardContainer, AppStyles.hCenter]}>
+            <CardWithImage
+                cardImage={NewPlanIcon}
+                text='Create a new plan'
+            />
+            <CardWithImage
+                cardImage={BrowseIcon}
+                text='Browse past plans'
+            />
+            <View style={styles.boxWithoutImageArea}>
+                <CardWithoutImage text='Journal' />
+                <CardWithoutImage text='Coloring Book' />
+            </View>
+        </View>
+    )
+}
 
 export default class SelfCareScreen extends Component {
     static navigationOptions = {
@@ -32,6 +80,7 @@ export default class SelfCareScreen extends Component {
                         headingImage={SelfCareIcon}
                         headingText='Self-Care'
                     />
+                    <CardContainer />
                 </ScrollView>
             </View>    
         )
@@ -39,5 +88,51 @@ export default class SelfCareScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        paddingHorizontal: 10,
+        backgroundColor: Colors.primaryBgColor
+    },
 
+    buttonWithImage: {
+        flexDirection: 'row',
+        height: height / 6,
+        marginTop: 15,
+        width: width - 40,
+        backgroundColor: Colors.lightGreen,
+        borderRadius: 4
+    },
+
+    cardImageArea: {
+        flex: .33
+    },
+
+    cardImageStyle: {
+        width: 60,
+        height: 60
+    },
+
+    cardTextArea: {
+        flex: .67
+    },
+
+    boxWithoutImageArea: {
+        flexDirection: 'row',
+        width: width - 40,
+        justifyContent: 'space-between'
+    },
+
+    buttonWithoutImage: {
+        height: height / 6,
+        width: width / 2 - 30,
+        marginTop: 15,
+        backgroundColor: Colors.lightGreen,
+        borderRadius: 4
+    },
+
+    buttonText: {
+        color: 'white',
+        fontWeight: '600',
+        fontSize: FontSizes.topicFS
+    }
 })
