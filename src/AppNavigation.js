79b916589d2169311a34e2ab1cad557f61706067
devac1103 <hdev0301@gmail.com>
2018-03-16 10:47:51 +0800

    Modified AppNavigation to have PastPlansScreen in the navigation flow and some code format change

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -7,26 +7,29 @@ import { StackNavigator } from 'react-navigation'
 //Screens
 import Home from './Screens/Home'
 import MainScreen from './Screens/MainScreen'
+import ChatScreen from './Screens/ChatScreen'
 import CallScreen from './Screens/CallScreen'
 import LearnScreen from './Screens/LearnScreen'
 import SelfCareScreen from './Screens/SelfCareScreen'
 import SearchScreen from './Screens/SearchScreen'
 import ExercisesScreen from './Screens/ExercisesScreen'
-import ChatScreen from './Screens/ChatScreen'
 import LearnDetailScreen from './Screens/LearnDetailScreen'
+import PastPlansScreen from './Screens/PastPlansScreen'
 
 export const AppNavigation =  StackNavigator({
     Home: { screen: Home },
     MainScreen: { screen: MainScreen },
+    ChatScreen: { screen: ChatScreen },
     CallScreen: { screen: CallScreen },
     LearnScreen: { screen: LearnScreen },
+    LearnDetailScreen: { screen: LearnDetailScreen },
     SelfCareScreen: { screen: SelfCareScreen },
+    PastPlansScreen: { screen: PastPlansScreen },
     SearchScreen: { screen: SearchScreen },
     ExercisesScreen: { screen: ExercisesScreen },
-    ChatScreen: { screen: ChatScreen },
-    LearnDetailScreen: { screen: LearnDetailScreen }
+
 },
 {
-  headerMode: 'none',
-  initialRouteName: 'MainScreen',
+    headerMode: 'none',
+    initialRouteName: 'MainScreen',
 })
