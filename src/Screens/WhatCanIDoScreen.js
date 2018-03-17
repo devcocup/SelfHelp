1a19da9da29dd65d82e50f6ff79168bdfa128694
devac1103 <hdev0301@gmail.com>
2018-03-17 15:08:39 +0800

    Modified WhatCanIDoScreen with all the elements implemented in the design, except the Continue button placement

--- src/Screens/WhatCanIDoScreen.js
+++ src/Screens/WhatCanIDoScreen.js
@@ -1,6 +1,6 @@
 // React
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -8,10 +8,38 @@ import Constants from '../Lib/Constants'
 
 // Assets
 import Header from '../Components/Header'
+import TopicButton from '../Components/TopicButton'
+import Button from '../Components/Button'
+
+const { height, width } = Dimensions.get('window')
+
+const DescriptionTextContainer = () => {
+    const textList = Constants.WhatCanIDoToHelpLabels.map((item, index) => {
+        return (
+            <Text
+                key={index}
+                style={styles.descriptionText}>
+                {item}
+            </Text>
+        )
+    })
+
+    return (
+        <View style={styles.descriptionTextArea}>
+            {textList}
+        </View>
+    )
+}
+
 
 export default class WhatCanIDoScreen extends Component {
+    onContinue = () => {
+        console.log('continue clicked')
+    }
+
     render() {
         const { navigation } = this.props
+        const { subTopic } = navigation.state.params
 
         return (
             <View style={AppStyles.mainContainer}>
@@ -19,7 +47,55 @@ export default class WhatCanIDoScreen extends Component {
                     type='Back'
                     navigation={navigation}
                 />
+                <ScrollView>
+                    <View style={[styles.container, AppStyles.hCenter]}>
+                        <View style={[styles.headingContainer, AppStyles.center]}>
+                            <Text style={styles.headerText}>
+                                {subTopic}
+                            </Text>
+                        </View>
+                        <TopicButton
+                            text='Introduction:'
+                        />
+                        <DescriptionTextContainer />
+                        <Button
+                            label="Continue"
+                            bgColor="white"
+                            onPress={this.onContinue}
+                        />
+                    </View>
+                </ScrollView>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    headingContainer: {
+        height: height / 8,
+        width,
+        marginTop: 15
+    },
+
+    headerText: {
+        color: 'white',
+        fontSize: Constants.FontSizes.headingFS,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+
+    descriptionTextArea: {
+        width,
+        paddingHorizontal: 30,
+        marginTop: 20
+    },
+
+    descriptionText: {
+        color: 'white',
+        marginBottom: 20
+    }
+})
