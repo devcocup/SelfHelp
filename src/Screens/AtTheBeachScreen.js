93091638b8d2e7b081a9af3308f8eaa9c737d516
devac1103 <hdev0301@gmail.com>
2018-03-21 04:27:43 +0800

    Added AtTheBeachScreen skeleton

---
+++ src/Screens/AtTheBeachScreen.js
@@ -0,0 +1,24 @@
+// React
+import React, { Component } from 'react'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+// Assets
+import Header from '../Components/Header'
+
+export default class AtTheBeachScreen extends Component {
+    render() {
+        return (
+            <View>
+                At The Beach Screen
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
