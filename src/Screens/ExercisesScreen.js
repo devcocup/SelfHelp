09f781058860ef38f66d255d5054f9004dbe847b
devac1103 <hdev0301@gmail.com>
2018-03-21 00:02:38 +0800

    Modified ExercisesScreen with the card contents

--- src/Screens/ExercisesScreen.js
+++ src/Screens/ExercisesScreen.js
@@ -1,20 +1,86 @@
 //React
 import React, { Component } from 'react'
-import { View,Text  } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+// Assets
+import Header from '../Components/Header'
+import HeadingContainer from '../Components/HeadingContainer'
+import SingleCard from '../Components/SingleCard'
+
+const ExercisesIcon = require('../Assets/Images/exercises_orange.png')
+
+const { height, width } = Dimensions.get('window')
+
+const CardContainer = ({ navigation }) => {
+    const cardList = Constants.ExercisesLabels.map((item, index) => {
+        return (
+            <View
+                key={index}
+                style={[styles.cardContainer, AppStyles.hCenter]}
+            >
+            {
+                item.content.map((cardItem, cardIndex) => {
+                    return (
+                        <SingleCard
+                            key={cardIndex}
+                            cardContent={cardItem}
+                        />
+                    )
+                })
+            }
+            </View>
+        )
+    })
+
+    return (
+        <View>{cardList}</View>
+    )
+}
+
 
 export default class ExercisesScreen extends Component {
-    static navigationOptions = {
-        title: 'Call Screen',
-        headerStyle: {backgroundColor: 'rgb(0,143,120)'},
-        headerTintColor: 'white',
-        headerTitleStyle : {alignSelf:'flex-start'}
-    }
-
     render() {
+        const { navigation } = this.props
+
         return(
-           <View>
-               <Text style={{ fontSize: 20 }}>Exercises Screen </Text>
-           </View>    
+            <View style={AppStyles.mainContainer}>
+                <Header
+                    type='Home'
+                    navigation={navigation}
+                />
+                <View style={{ flex: 1 }}>
+                    <ScrollView>
+                        <HeadingContainer
+                            headingImage={ExercisesIcon}
+                            headingText='Excercises'
+                        />
+                        <CardContainer
+                            navigation={navigation}
+                        />
+                    </ScrollView>
+                </View>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        paddingHorizontal: 10,
+        backgroundColor: Constants.Colors.primaryBgColor
+    },
+
+    singleCardContainer: {
+        flexDirection: 'row',
+        height: height / 7,
+        width: width - 20,
+        marginTop: Constants.Margins.elementMT,
+        backgroundColor: Constants.Colors.secondaryBgColor,
+        borderRadius: Constants.BorderRadii.boxBR
+    }
+})
