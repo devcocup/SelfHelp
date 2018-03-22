1721f7730142eac6cd4f9d669f5be1c0fb2314f9
devac1103 <hdev0301@gmail.com>
2018-03-23 06:37:13 +0800

    Modified JournalScreen to have head card for prompt content

--- src/Screens/JournalScreen.js
+++ src/Screens/JournalScreen.js
@@ -1,18 +1,55 @@
 // React
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, ScrollView, StyleSheet, Dimensions } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
 import Constants from '../Lib/Constants'
 
-// 
+// Assets
+import Header from '../Components/Header'
+import HeadingContainer from '../Components/HeadingContainer'
+import JournalHeadCard from '../Components/JournalHeadCard'
+
+const JournalIcon = require('../Assets/Images/journal_orange.png')
+
+const { height, width } = Dimensions.get('window')
+const cardHeadTitle = 'Current Journal Prompt'
+const cardContentText = 'How are you feeling today?'
+
+const CardContainer = ({ navigation }) => {
+    return (
+        <View style={AppStyles.hCenter}>
+            <JournalHeadCard
+                topic={cardHeadTitle}
+                content={cardContentText}
+            />
+        </View>
+    )
+}
+
 
 export default class JournalScreen extends Component {
     render() {
+        const { navigation } = this.props
+    
         return (
             <View style={AppStyles.mainContainer}>
-
+                <Header
+                    type='Home'
+                    navigation={navigation}
+                />
+                <View style={{ flex: 1 }}>
+                    <ScrollView>
+                        <HeadingContainer
+                            headingImage={JournalIcon}
+                            headingText='Journal'
+                        />
+                        <CardContainer
+                            navigation={navigation}
+                        />
+                    </ScrollView>
+                </View>
             </View>
         )
     }
