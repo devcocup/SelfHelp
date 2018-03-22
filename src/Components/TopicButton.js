97e621e930d67520b5af2dfdb872565bf88c5e4a
devac1103 <hdev0301@gmail.com>
2018-03-23 07:01:30 +0800

    Fixed code format

--- src/Components/TopicButton.js
+++ src/Components/TopicButton.js
@@ -1,12 +1,17 @@
+// React
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 
-const { height, width } = Dimensions.get("window")
+// Global Styles & Constants
+import Constans from '../Lib/Constants'
+import AppStyles from '../Lib/AppStyles'
+
+const { height, width } = Dimensions.get('window')
 
 const TopicButton = ({ text }) => {
     return (
         <TouchableOpacity>
-            <View style={styles.button}>
+            <View style={[styles.button, AppStyles.center]}>
                 <Text style={styles.buttonText}>{text}</Text>
             </View>
         </TouchableOpacity>
@@ -18,15 +23,13 @@ const styles = StyleSheet.create({
         height: height / 16,
         marginTop: 15,
         width: width - 40,
-        backgroundColor: "#F7A553",
-        alignItems: "center",
-        justifyContent: "center",
+        backgroundColor: '#F7A553',
         borderRadius: 4
     },
 
     buttonText: {
         color: 'white',
-        fontWeight: "600",
+        fontWeight: '600',
         fontSize: 18
     }
 })
