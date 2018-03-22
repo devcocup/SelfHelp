e9497ad90e6d61390f06658fc5dd5d10f358d0cd
devac1103 <hdev0301@gmail.com>
2018-03-22 23:37:45 +0800

    Modified SoothingSoundsScreen to have the list of content displayed

--- src/Screens/SoothingSoundsScreen.js
+++ src/Screens/SoothingSoundsScreen.js
@@ -8,6 +8,38 @@ import Constants from '../Lib/Constants'
 
 // Assets
 import Header from '../Components/Header'
+import TopicButton from '../Components/TopicButton'
+import SingleCard from '../Components/SingleCard'
+
+const CardContainer = ({ navigation }) => {
+    const subTopic = navigation.state.params.subTopic
+
+    const cardList = Constants.SoothingSoundsLabels.map((item, index) => {
+        return (
+            <View
+                key={index}
+                style={[styles.CardContainer, AppStyles.hCenter]}
+            >
+                <TopicButton text={subTopic} />
+                {
+                    item.content.map((cardItem, cardIndex) => {
+                        return (
+                            <SingleCard
+                                key={cardIndex}
+                                cardContent={cardItem}
+                                onPress={() => onCardSelected((cardItem.subTopic, navigation))}
+                            />
+                        )
+                    })
+                }
+            </View>
+        )
+    })
+
+    return (
+        <View>{cardList}</View>
+    )
+}
 
 
 export default class SoothingSoundsScreen extends Component {
@@ -20,6 +52,13 @@ export default class SoothingSoundsScreen extends Component {
                     type='Back'
                     navigation='navigation'
                 />
+                <View style={{ flex: 1 }}>
+                    <ScrollView>
+                        <CardContainer
+                            navigation={navigation}
+                        />
+                    </ScrollView>
+                </View>
             </View>
         )
     }
