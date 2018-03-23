90792692c30da66a1d0c0ca6ac8ba5b4256f4804
devac1103 <hdev0301@gmail.com>
2018-03-23 23:23:10 +0800

    Added CurrentJournalPromptScreen for answer current journal

---
+++ src/Screens/CurrentJournalPromptScreen.js
@@ -0,0 +1,26 @@
+// React
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+// Assets
+import Header from '../Components/Header'
+
+
+export default class CurrentJournalPromptScreen extends Component {
+    render() {
+        const { navigation } = this.props
+
+        return (
+            <View style={AppStyles.mainContainer}>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
