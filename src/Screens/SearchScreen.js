be6180903378e49f31c470d517903d5c36412423
devac1103 <hdev0301@gmail.com>
2018-03-25 16:42:30 +0800

    Modified SearchScreen layout with user defined paddings

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,6 +1,6 @@
 //React
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -8,20 +8,48 @@ import Constants from '../Lib/Constants'
 
 // Assets
 import Header from '../Components/Header'
+import HeadingContainer from '../Components/HeadingContainer'
+import CardWithImage from '../Components/CardWithImage'
 
 const SearchIcon = require('../Assets/Images/search_orange.png')
 
 const { height, width } = Dimensions.get('window')
+const { SearchLabels, Paddings, Colors } = Constants
+
+const onPress = (navigation) => {
+    console.log('card clicked')
+}
+
+const CardContainer = ({ navigation }) => {
+    const cardList = SearchLabels.map((item, index) => {
+        return (
+            <View
+                key={index}
+                style={[styles.cardContainer, AppStyles.hCenter]}
+            >
+                {
+                    item.content.map((cardItem, cardIndex) => {
+                        return (
+                            <CardWithImage
+                                key={cardIndex}
+                                cardImage={cardItem.icon}
+                                text={cardItem.label}
+                                onPress={() => onPress(navigation)}
+                            />
+                        )
+                    })
+                }
+            </View>
+        )
+    })
+
+    return (
+        <View>{cardList}</View>
+    )
+}
 
 
 export default class SearchScreen extends Component {
-    static navigationOptions = {
-        title: 'Call Screen',
-        headerStyle: {backgroundColor: 'rgb(0,143,120)'},
-        headerTintColor: 'white',
-        headerTitleStyle : {alignSelf:'flex-start'},
-    }
-
     render() {
         const { navigation } = this.props
 
@@ -31,11 +59,25 @@ export default class SearchScreen extends Component {
                     type='Home'
                     navigation={navigation}
                 />
+                <ScrollView>
+                    <HeadingContainer
+                        headingImage={SearchIcon}
+                        headingText='Search'
+                    />
+                    <CardContainer
+                        navigation={navigation}
+                    />
+                </ScrollView>
             </View>    
         )
     }
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    cardContainer: {
+        flex: 1,
+        paddingHorizontal: Paddings.elementP,
+        paddingVertical: Paddings.elementP,
+        backgroundColor: Colors.primaryBgColor
+    }
 })
