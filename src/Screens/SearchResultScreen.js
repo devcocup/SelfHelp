1bc4a5389985d44954a7e5a1eca8b5e568adb4d6
devac1103 <hdev0301@gmail.com>
2018-03-28 10:31:33 +0800

    Modified SearchResultScreen to display the list of items from constant

--- src/Screens/SearchResultScreen.js
+++ src/Screens/SearchResultScreen.js
@@ -1,6 +1,7 @@
 // React
 import React, { Component } from 'react'
-import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import Accordion from 'react-native-collapsible/Accordion'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -8,6 +9,70 @@ import Constants from '../Lib/Constants'
 
 // Assets
 import Header from '../Components/Header'
+
+const DownArrowIcon = require('../Assets/Images/down_arrow.png')
+
+const { height, width } = Dimensions.get('window')
+const {
+    SearchResultLabels,
+    Paddings,
+    Colors,
+    FontSizes
+} = Constants
+
+
+const cardContent = () => {
+    return (
+        <View>
+            {
+
+            }
+        </View>
+    )
+}
+
+const ListContainer = ({ navigation }) => {
+    const list = SearchResultLabels.map((item, index) => {
+        return (
+            <View
+                key={index}
+                style={[styles.cardContainer, AppStyles.hCenter]}
+            >
+            {
+                item.content.map((cardItem, cardIndex) => {
+                    return (
+                        <View
+                            key={cardIndex}
+                            style={[styles.cardItem, AppStyles.hCenter]}>
+                            <View style={styles.cardTitleArea}>
+                                <View style={[styles.checkMarkArea, AppStyles.center]}>
+                                    <TouchableOpacity
+                                        onPress={() => onItemSelected}
+                                    >
+                                        <Image source={DownArrowIcon} style={styles.checkMark} />
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={styles.textArea}>
+                                    <Text style={styles.cardText}>{ cardItem.label }</Text>
+                                </View>
+                            </View>
+                            {
+                                <View>
+                                    {cardContent}
+                                </View>
+                            }
+                        </View>
+                    )
+                })
+            }
+            </View>
+        )
+    })
+
+    return (
+        <View>{list}</View>
+    )
+}
 
 
 export default class SearchResultScreen extends Component {
@@ -20,11 +85,46 @@ export default class SearchResultScreen extends Component {
                     type='Back'
                     navigation={navigation}
                 />
+                <ScrollView>
+                    <ListContainer
+                        navigation={navigation}
+                    />
+                </ScrollView>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    cardContainer: {
+        width
+    },
 
+    cardItem: {
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth: 2,
+        borderBottomColor: Colors.lightGreen,
+        paddingVertical: Paddings.listPV,
+        paddingHorizontal: Paddings.listPH
+    },
+
+    checkMarkArea: {
+        flex: .15
+    },
+
+    checkMark: {
+        width: 40,
+        height: 40
+    },
+
+    textArea: {
+        flex: .85
+    },
+
+    cardText: {
+        color: 'white',
+        fontSize: FontSizes.listFS,
+        fontWeight: '600'
+    }
 })
