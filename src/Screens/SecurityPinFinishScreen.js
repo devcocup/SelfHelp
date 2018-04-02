3f63cdd320f12ee432a5e6126119f26d66723c64
devac1103 <hdev0301@gmail.com>
2018-04-02 11:48:49 +0800

    Modified SecurityPinFinishScreen UI as in the design

--- src/Screens/SecurityPinFinishScreen.js
+++ src/Screens/SecurityPinFinishScreen.js
@@ -1,22 +1,105 @@
 // React
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
 import Constants from '../Lib/Constants'
 
+// Assets
+import SecurityPinHeader from '../Components/SecurityPinHeader'
+import Button from '../Components/Button'
+
+const { height, width } = Dimensions.get('window')
+const { Margins, Paddings, FontSizes, Colors, BorderRadii } = Constants
+
 
 export default class SecurityPinFinishScreen extends Component {
+    goToScreen = (ScreenName, navigation) => {
+        const { navigate } = navigation
+        navigate(ScreenName)
+    }
+
+    onDoneClicked = (navigation) => {
+        this.goToScreen('JournalScreen', navigation)
+    }
+
     render() {
+        const { navigation } = this.props
+        const pinNumber = '474789'
+        const descriptionHeaderText = 'Write it down in a safe place'
+        const descriptionContentText = 'If you can\'t remember your pin,\
+        then you can reset it by answering your security question. If you cannot\
+        remember your security question answer or your pin, then you have the option\
+        to reset your pin, but all your data on this app will be erased.'
+
         return (
             <View style={AppStyles.mainContainer}>
-                
+                <SecurityPinHeader
+                    headerType='FLOW'
+                    flowIndex={5}
+                    navigation={navigation}
+                />
+                <ScrollView>
+                    <View style={AppStyles.hCenter}>
+                        <View style={styles.statusArea}>
+                            <Text style={styles.headerText}>All done!</Text>
+                        </View>
+                        <View style={styles.pinNumberArea}>
+                            <Text style={styles.headerText}>Your pin is:</Text>
+                            <View style={styles.pinNumberBox}>
+                                <Text style={styles.headerText}>{pinNumber}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.descriptionArea}>
+                            <Text style={styles.headerText}>{descriptionHeaderText}</Text>
+                            <Text style={styles.contentText}>{descriptionContentText}</Text>
+                        </View>
+                        <View>
+                            <Button
+                                label='Done'
+                                bgColor='white'
+                                onPress={() => this.onDoneClicked(navigation)}
+                            />
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    statusArea: {
+        paddingVertical: Paddings.containerP
+    },
 
+    headerText: {
+        color: 'white',
+        fontSize: FontSizes.pinHeaderFS,
+        fontWeight: '600',
+        textAlign: 'center'
+    },
+
+    pinNumberArea: {
+        paddingVertical: Paddings.containerP
+    },
+
+    pinNumberBox: {
+        backgroundColor: Colors.orange,
+        marginTop: Margins.elementMT,
+        padding: Paddings.elementP,
+        borderRadius: BorderRadii.boxBR
+    },
+
+    descriptionArea: {
+        paddingTop: Paddings.lP
+    },
+
+    contentText: {
+        color: 'white',
+        fontSize: FontSizes.pinContentFS,
+        paddingHorizontal: Paddings.lP,
+        marginVertical: Margins.containerM
+    }
 })
