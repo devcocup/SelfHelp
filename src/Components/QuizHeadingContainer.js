2ef170d6519eef50a3158422ceb78cd0fd4b8db7
devac1103 <hdev0301@gmail.com>
2018-04-08 10:25:45 +0800

    Added custom QuizHeadingContainer component

---
+++ src/Components/QuizHeadingContainer.js
@@ -0,0 +1,56 @@
+// React
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+// Assets
+
+const { height, width } = Dimensions.get('window')
+const { Colors, FontSizes } = Constants
+
+
+const QuizHeadingContainer = ({ quizTitle, quizSubTitle }) => {
+    return (
+        <View style={[styles.headingContainer, AppStyles.center]}>
+            <Text style={styles.titleText}>
+                {quizTitle}
+            </Text>
+            <View style={styles.separateBar}></View>
+            <Text style={styles.subTitleText}>
+                {quizSubTitle}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headingContainer: {
+        height: height / 4,
+        width,
+        backgroundColor: 'white'
+    },
+
+    titleText: {
+        fontSize: FontSizes.quizTitleFS,
+        fontWeight: '600',
+        marginBottom: 10
+    },
+
+    separateBar: {
+        height: 2,
+        width: width - 60,
+        backgroundColor: Colors.orange
+    },
+
+    subTitleText: {
+        color: Colors.gray,
+        fontSize: FontSizes.quizTitleFS,
+        fontWeight: '600',
+        marginTop: 10
+    }
+})
+
+export default QuizHeadingContainer
