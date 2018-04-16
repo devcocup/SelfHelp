9d708813c6faee89535d50f018bafd02121bce49
devac1103 <hdev0301@gmail.com>
2018-04-16 17:08:24 +0800

    Modified ExerciseListenScreen to test sound playing feature

--- src/Screens/ExerciseListenScreen.js
+++ src/Screens/ExerciseListenScreen.js
@@ -1,6 +1,12 @@
 // React
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, ScrollView, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, ImageBackground, ScrollView, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import SoundPlayer from 'react-native-sound-player'
+import AudioPlayer from 'react-native-play-audio'
+import {
+    Player,
+    MediaStates
+} from 'react-native-audio-toolkit'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -10,19 +16,99 @@ import Constants from '../Lib/Constants'
 import Header from '../Components/Header'
 
 const { height, width } = Dimensions.get('window')
-
-const AudioControl = () => {
-    return (
-        <View style={[styles.audioControl, AppStyles.center]}>
-            <Text style={styles.textStyle}>AUDIO CONTROLS</Text>
-        </View>
-    )
-}
+const { FontSizes, Colors, Paddings } = Constants
 
 
 export default class ExerciseListenScreen extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            isPlaying: false,
+            playButtonText: 'Play'
+        }
+    }
+
+    componentDidMount() {
+        // subscribe to the finished playing event
+        SoundPlayer.onFinishedPlaying((success: boolean) => { // success is true when the sound is played
+            console.log('finished playing', success);
+        });
+    }
+
+    componentWillUnmount() {
+        // this.player = null;
+        // unsubscribe when unmount
+        SoundPlayer.unmount();
+        // AudioPlayer.onEnd(() => {
+        //     console.log('on end');
+        // })
+    }
+
+    controlSound() {
+        const { isPlaying } = this.state
+
+        // const url = 'http://d2gjx9ybupbr1n.cloudfront.net/Chimes.mp3'
+        // const url = 'test.mp3'
+
+        // this.player = new Player(url)
+
+        // this.player.prepare((err) => {
+        //     let seconds = this.player.duration / 1000; //duration is in milliseconds
+        //     this.setState({
+        //         totalTime: seconds
+        //     })
+        // })
+
+        // this.player.play(() => {
+        //     this.setState({
+        //         isPlaying: true
+        //     })
+        // })
+
+        // AudioPlayer.prepare(url, () => {
+            // AudioPlayer.play()
+
+            // AudioPlayer.getDuration((duration) => {
+            //     console.log(duration)
+            // })
+
+            // setInterval(() => {
+            //     AudioPlayer.getCurrentTime((currentTime) => {
+            //         console.log(currentTime)
+            //     })
+            // }, 1000);
+            // AudioPlayer.stop()
+            // AudioPlayer.pause()
+            // AudioPlayer.setCurrentTime(50.5)
+        // })
+
+        if (isPlaying) {
+            try {
+              SoundPlayer.playSoundFile('test', 'mp3');
+            } catch (e) {
+              console.log(`cannot play the sound file`, e);
+            }
+        }
+    }
+
+    onPlayButtonClicked = () => {
+        // this.setState({
+        //     isPlaying: !this.state.isPlaying
+        // })
+        // this.controlSound()
+        try {
+            SoundPlayer.playSoundFile('test', 'mp3');
+        } catch (e) {
+            console.log(`cannot play the sound file`, e);
+        }
+    }
+
+
     render() {
+        const { onPlayButtonClicked } = this
         const { navigation } = this.props
+        const { playButtonText } = this.state
         const { params } = navigation.state
         const { bgImage } = params
 
@@ -36,7 +122,13 @@ export default class ExerciseListenScreen extends Component {
                     navigation={navigation}
                 />
                 <View style={[{flex: 1}, AppStyles.vEnd]}>
-                    <AudioControl />
+                    <View style={[styles.audioControl, AppStyles.center]}>
+                        <TouchableOpacity onPress={onPlayButtonClicked}>
+                            <View style={[AppStyles.center, styles.buttonArea]}>
+                                <Text style={styles.playButtonText}>{playButtonText}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </ImageBackground>
         )
@@ -47,11 +139,17 @@ const styles = StyleSheet.create({
     audioControl: {
         height: height / 8,
         width,
-        backgroundColor: Constants.Colors.gray
+        backgroundColor: Colors.gray
     },
 
     textStyle: {
         color: 'white',
-        fontSize: Constants.FontSizes.topicFS
+        fontSize: FontSizes.topicFS
+    },
+
+    playButtonText: {
+        color: 'white',
+        fontSize: FontSizes.topicFS,
+        fontWeight: 'bold'
     }
 })
