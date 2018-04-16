54bdd29681a5e3cb957f4b649eda8e5765ee913c
devac1103 <hdev0301@gmail.com>
2018-04-16 17:07:05 +0800

    Modified CallScreen to have phone number input field and phone call feature

--- src/Screens/CallScreen.js
+++ src/Screens/CallScreen.js
@@ -1,7 +1,20 @@
 //React
 import React, { Component } from 'react'
-import { View,Text, TouchableOpacity  } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import Communications from 'react-native-communications'
 import { NavigationActions } from 'react-navigation'
+import PhoneInput from 'react-native-phone-input'
+
+// Global Styles & Constants
+import AppStyles from '../Lib/AppStyles'
+import Constants from '../Lib/Constants'
+
+// Assets
+import Header from '../Components/Header'
+
+const { height, width } = Dimensions.get('window')
+const { Paddings, Margins, Colors, FontSizes, BorderRadii } = Constants
+
 
 export default class CallScreen extends Component {
     static navigationOptions = {
@@ -11,12 +24,61 @@ export default class CallScreen extends Component {
         headerTitleStyle : {alignSelf:'flex-start'}
     }
 
+    callPhone = () => {
+        const { formattedNumber } = this.phone.state
+        Communications.phonecall(formattedNumber, true)
+    }
+
     render() {
+        const { navigation } = this.props
+
         return(
-           <View style={{ backgroundColor: 'green',flex:1 }}>
-               <Text style={{ fontSize: 30, color: 'white' }}>Call Screen </Text>
+           <View style={AppStyles.mainContainer}>
+                <Header
+                    type='Back'
+                    navigation={navigation}
+                />
+                <View style={[styles.container, AppStyles.hCenter]}>
+                    <View style={[styles.numberField, AppStyles.vCenter]}>
+                        <PhoneInput
+                            ref={(ref) => { this.phone = ref }}
+                        />
+                    </View>
+                    <TouchableOpacity onPress={this.callPhone}>
+                        <View style={[styles.callButton, AppStyles.center]}>
+                            <Text style={styles.callButtonText}>Call</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
            </View>    
 
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: Paddings.lP
+    },
+    
+    numberField: {
+        width: width - 80,
+        height: 40,
+        borderRadius: BorderRadii.boxBR,
+        backgroundColor: 'white'
+    },
+
+    callButton: {
+        width: width / 3,
+        height: 50,
+        backgroundColor: Colors.red,
+        borderRadius: BorderRadii.buttonBR,
+        marginTop: Margins.elementMT
+    },
+
+    callButtonText: {
+        color: 'white',
+        fontSize: FontSizes.contentFS,
+        fontWeight: 'bold'
+    }
+})
