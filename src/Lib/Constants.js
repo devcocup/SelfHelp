db587cd328756ef5911353e33ef11e2550027c7b
devac1103 <hdev0301@gmail.com>
2018-04-17 01:01:20 +0800

    Modified Quiz label constants

--- src/Lib/Constants.js
+++ src/Lib/Constants.js
@@ -372,12 +372,98 @@ const QuizLabels = [
                 choiceCaption: 'B',
                 content: 'Interrupt the conversation and tell them that what \
                 they\'re saying isn\'t okay: \"Why would you say that about Sue or Julia? \
-                You would\'t say that about me. That\'s messed up.\"'
+                You would\'t say that about me. That\'s messed up.\"',
+                subContent: 'Asking why someone is saying these things is a good lead into telling them why what they\'re saying is harmful. As a peer they may be more likely to listen to what you have to say.'
             },
             {
                 choiceCaption: 'C',
                 content: 'Tell your commanding officer or your training instructor \
-                about what you witnessed.'
+                about what you witnessed.',
+                subContent: 'What your coworkers are saying about other members in your unit undermines the strength of your team and can lead to a hostile working environment. Your CO may have additional ways to address the problem at hand and make it a safer working environment for everyone in your unit'
+            }
+        ]
+    },
+    {
+        title: 'Harassment',
+        question: 'Your friend confides in you that a coworker has been making comments about how attractive she is, even after she asked him to stop, and the other day blocked the door of the breakroom so that the only way she could leave was to press against him through the doorway. She says that it\'s been making her nervous about what they\'ll do next and she\'s not sure how to stop it. As she tells you more, you realize that she\'s talking about someone in your unit. What can you do to help?',
+        answers: [
+            {
+                choiceCaption: 'A',
+                content: 'Let them know that what\'s happening to them isn\'t their fault, and that you\'re there to support them. Ask if there\'s a particular place where this person usually approaches them and offer to accompany them.',
+                subContent: 'This is a good option and shows that you care about your friend. Brainstorming options shows that you take your friend\'s situation seriously and acknowledges the ways that the harassment is affecting their life.'
+            },
+            {
+                choiceCaption: 'B',
+                content: 'Talk to the guy harassing your friend. Tell them that what they\'re doing is unacceptable. Not only that, it affects your unit\'s readiness and is punishable.',
+                subContent: 'This could be a difficult conversation, but he might not know how he has been coming across. This option should only be taken if the friend that confided in you is comfortable with it, because it may cause more problems for you or your friend if the harasser responds negatively.'
+            },
+            {
+                choiceCaption: 'C',
+                content: 'Offer to go with your friend to meet with a professional resource. (Sexual Assault Response Coordinator (SARC), Sexual Assault Prevention and Response Victim Advocate (SAPR VA), an Equal Employment Opportunity Counselor, Special Victims Counsel or Victims Legal Counsel, Chaplain)',
+                subContent: 'It\'s important to let your friend make the next decision that they feel most comfortable making. The fact that your friend told you about what has been going on was really brave, and they may be open to additional support. Offer to reach out to support personnel for them.  Asking them how you can support them can be empowering and help them regain control of the situation.'
+            }
+        ]        
+    },
+    {
+        title: 'Hazing',
+        question: 'A new soldier was recently transferred from another base to your unit, so everyone is in the barracks drinking and being loud. It\'s tradition. But the last time that happened, things got weird at the end of the night. The staff sergeant had the new guy naked and doing push ups. You went to bed early, but you heard some messed up stuff about what happened after that. The staff sergeant is already inebriated and you\'re worried about what might happen tonight. What can you do to help?',
+        answers: [
+            {
+                choiceCaption: 'A',
+                content: 'Confer with your peers. \"Guys, let\'s make sure things don\'t end up like last time.\"',
+                subContent: 'Sometimes, it just takes one person to break through the groupthink that can take over a large group of people.  It can be helpful to approach people individually, rather than all at once, both for your own safety and to get individual opinions.',
+            },
+            {
+                choiceCaption: 'B',
+                content: 'Get help if things go too far.',
+                subContent: 'Find someone that can help you intervene in the situation if you don\'t feel safe doing so. This may be another senior Service member that you trust, your CO, a SARC or another professional. This is a good option as it can help keep you safe, and bring others into the fold about what\'s going on.'
+            },
+            {
+                choiceCaption: 'C',
+                content: 'Find an opportunity to get them out of harm\'s way.',
+                subContent: 'You know the situation best. Taking into consideration what\'s going on, find an opportunity to remove the person from the situation. This might also be an opportunity to look for long-term solutions to address what has been happening.'
+            }
+        ]
+    },
+    {
+        title: 'Sexual abuse',
+        question: 'A friend confides in you that he\'s being forced to have sex with someone. He jokes about it at first, but you can tell it really bothers him.  He shares that the person is a woman, and an officer, but won\'t say who. He says he\'s ashamed and has no idea how make it stop. He\'s afraid that no one will believe him because he\'s a man, and why wouldn\'t he want to have sex with her. What can you do to help?',
+        answers: [
+            {
+                choiceCaption: 'A',
+                content: '\"I believe you.\"',
+                subContent: 'Some of the most important words that a survivor can hear is \"I believe you.\" Validate how they\'re feeling and offer your support to get them the help they need. Sexual assault isn\'t something that only happens to women, or can only be perpetrated by men.'
+            },
+            {
+                choiceCaption: 'B',
+                content: 'Suggest that they talk to a supervisor they are comfortable approaching.',
+                subContent: 'Please note that if the supervisor they reach out to is in their chain of command, it will automatically trigger an Unrestricted Report. This is a good option if your friend is ready to file an Unrestricted report against their abuser. You can learn more about Restricted and Unrestricted Report here, or speak to a Safe Helpline staff member at online.safehelpline.org or by calling 877-995-5247.'
+            },
+            {
+                choiceCaption: 'C',
+                content: 'Offer to go with them to see a Sexual Assault Response Coordinator (SARC) or Sexual Assault Prevention and Response Victim Advocate (SAPR VA).',
+                subContent: 'This is a good option. SARCs are a confidential resource who ensure that survivors receive appropriate and responsive care. SARCs manage an installation or unit\'s Sexual Assault Prevention and Response (SAPR) program, and serve as the single point of contact to coordinate care. They will be able to talk through different reporting options with your friend, and how they can get long-term support.'
+            }
+        ]
+    },
+    {
+        title: 'Stalking',
+        question: 'Your friend shares that her ex-girlfriend, another Service member on base, won\'t stop calling or texting her, and has been \"dropping by\" her house after work. She says she feels unsafe and is having trouble sleeping and focusing at work, but she isn\'t sure she wants to report it. What can you do to help?',
+        answers: [
+            {
+                choiceCaption: 'A',
+                content: '\"That must be so frightening, I\'m sorry that she\'s acting that way.  Do you need me to stay over tonight?\" Ask if there\'s a particular time or day of the week her ex usually approaches them and offer to stay overnight.',
+                subContent: 'This is a good option and shows that you care about your friend. Offering to stay with them can help them feel safer. However, this is a temporary solution because you won\'t be able to be with your friend all the time.'
+            },
+            {
+                choiceCaption: 'B',
+                content: 'Ask her if she would consider telling her supervisor.',
+                subContent: 'Respect her decision to not want to report but suggest possibly confiding in her supervisor or another superior about what has been going to help her gain an ally in this situation.'
+            },
+            {
+                choiceCaption: 'C',
+                content: 'Suggest that she reach out to an advocate for support.',
+                subContent: 'Local rape crisis centers have resources to help victims of stalking and can help your friend with obtaining a protection order with law enforcement if she chooses to get one. You can look up local rape crisis centers here, but remember to let her decide if she wants to connect with them or not. You may also suggest that she reach out to a counselor at the Equal Employment Opportunity Commission.'
             }
         ]
     }
