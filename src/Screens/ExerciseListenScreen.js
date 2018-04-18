42a2fccdb77931444e49751fbff2791d95c5df0a
devac1103 <hdev0301@gmail.com>
2018-04-19 00:52:07 +0800

    Modified ExerciseListenScreen to enable sound playing feature using NativeModules

--- src/Screens/ExerciseListenScreen.js
+++ src/Screens/ExerciseListenScreen.js
@@ -1,8 +1,6 @@
 // React
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, ScrollView, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
-import SoundPlayer from 'react-native-sound-player'
-import AudioPlayer from 'react-native-play-audio'
+import { View, Text, ImageBackground, ScrollView, TouchableOpacity, StyleSheet, Dimensions, NativeModules } from 'react-native'
 
 // Global Styles & Constants
 import AppStyles from '../Lib/AppStyles'
@@ -13,6 +11,7 @@ import Header from '../Components/Header'
 
 const { height, width } = Dimensions.get('window')
 const { FontSizes, Colors, Paddings } = Constants
+const { RNSoundPlayer } = NativeModules
 
 
 export default class ExerciseListenScreen extends Component {
@@ -26,78 +25,42 @@ export default class ExerciseListenScreen extends Component {
     }
 
     componentDidMount() {
-        // subscribe to the finished playing event
-        SoundPlayer.onFinishedPlaying((success: boolean) => { // success is true when the sound is played
-            console.log('finished playing', success);
-        });
     }
 
     componentWillUnmount() {
-        // this.player = null;
-        // unsubscribe when unmount
-        SoundPlayer.unmount();
-        // AudioPlayer.onEnd(() => {
-        //     console.log('on end');
-        // })
     }
 
     controlSound() {
         const { isPlaying } = this.state
 
-        // const url = 'http://d2gjx9ybupbr1n.cloudfront.net/Chimes.mp3'
-        // const url = 'test.mp3'
-
-        // this.player = new Player(url)
-
-        // this.player.prepare((err) => {
-        //     let seconds = this.player.duration / 1000; //duration is in milliseconds
-        //     this.setState({
-        //         totalTime: seconds
-        //     })
-        // })
-
-        // this.player.play(() => {
-        //     this.setState({
-        //         isPlaying: true
-        //     })
-        // })
-
-        // AudioPlayer.prepare(url, () => {
-            // AudioPlayer.play()
-
-            // AudioPlayer.getDuration((duration) => {
-            //     console.log(duration)
-            // })
-
-            // setInterval(() => {
-            //     AudioPlayer.getCurrentTime((currentTime) => {
-            //         console.log(currentTime)
-            //     })
-            // }, 1000);
-            // AudioPlayer.stop()
-            // AudioPlayer.pause()
-            // AudioPlayer.setCurrentTime(50.5)
-        // })
-
         if (isPlaying) {
+            this.setState({
+                playButtonText: 'Pause'
+            })
+
             try {
-              SoundPlayer.playSoundFile('test', 'mp3');
+                RNSoundPlayer.playSoundFile('test', 'mp3')
             } catch (e) {
-              console.log(`cannot play the sound file`, e);
+                console.log(`cannot play the sound file`, e);
+            }
+        } else {
+            this.setState({
+                playButtonText: 'Play'
+            })
+
+            try {
+                RNSoundPlayer.pause()
+            } catch (e) {
+                console.log(`cannot play the sound file`, e);
             }
         }
     }
 
     onPlayButtonClicked = () => {
-        // this.setState({
-        //     isPlaying: !this.state.isPlaying
-        // })
-        // this.controlSound()
-        try {
-            SoundPlayer.playSoundFile('test', 'mp3');
-        } catch (e) {
-            console.log(`cannot play the sound file`, e);
-        }
+        this.setState({
+            isPlaying: !this.state.isPlaying
+        })
+        this.controlSound()
     }
 
 
@@ -118,7 +81,7 @@ export default class ExerciseListenScreen extends Component {
                     navigation={navigation}
                 />
                 <View style={[{flex: 1}, AppStyles.vEnd]}>
-                    <View style={[styles.audioControl, AppStyles.center]}>
+                    <View style={[styles.audioControl, AppStyles.hCenter]}>
                         <TouchableOpacity onPress={onPlayButtonClicked}>
                             <View style={[AppStyles.center, styles.buttonArea]}>
                                 <Text style={styles.playButtonText}>{playButtonText}</Text>
@@ -143,9 +106,13 @@ const styles = StyleSheet.create({
         fontSize: FontSizes.topicFS
     },
 
+    buttonArea: {
+        paddingTop: Paddings.elementP
+    },
+
     playButtonText: {
         color: 'white',
-        fontSize: FontSizes.topicFS,
+        fontSize: FontSizes.menuFS,
         fontWeight: 'bold'
     }
 })
