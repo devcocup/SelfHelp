c9da2374b6b8962f1bf940c4d63d0c83e5d42f81
devac1103 <hdev0301@gmail.com>
2018-04-26 09:15:35 +0800

    Modified SelfCareQuizResultsScreen to have the layout as in the design

--- src/Screens/SelfCareQuizResultsScreen.js
+++ src/Screens/SelfCareQuizResultsScreen.js
@@ -9,25 +9,49 @@ import Constants from '../Lib/Constants'
 // Assets
 import Header from '../Components/Header'
 import  QuizHeadingContainer from '../Components/QuizHeadingContainer'
+import CardWithImage from '../Components/CardWithImage'
+
+const PhoneIcon = require('../Assets/Images/phone.png')
 
 const { height, width } = Dimensions.get('window')
-const { QuizResultsLabels, Margins, Paddings, Colors, FontSizes } = Constants
+const { QuizResultsLabels, Margins, Paddings, Colors, FontSizes, BorderRadii } = Constants
 
 const StatusCard = ({ status, frequency, onPress }) => {
+    let bgColor = Colors.orange
+
+    switch (frequency) {
+        case 'Never':
+            bgColor = Colors.orange
+            break
+        case 'Sometimes':
+            bgColor = Colors.secondaryOrange
+            break
+        case 'Always':
+            bgColor = Colors.secondaryGray
+            break
+        default:
+            bgColor = Colors.orange
+            break
+    }
+
     return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.cardStyle}>
-                <Text style={styles.statusText}>{status}</Text>
-                <View style={styles.separator}></View>
-                <Text style={styles.frequencyText}>{frequency}</Text>
-            </View>
+        <TouchableOpacity style={[styles.cardStyle, AppStyles.center, {backgroundColor: bgColor}]}>
+            <Text style={styles.statusText}>{status}</Text>
+            <View style={styles.separator}></View>
+            <Text style={styles.frequencyText}>{frequency}</Text>
         </TouchableOpacity>
     )
 }
 
 
 export default class SelfCareQuizResultScreen extends Component {
+    callPhone = (navigation) => {
+        const { navigate } = navigation
+        navigate('TalkToSomeoneScreen')
+    }
+
     render() {
+        const { callPhone } = this 
         const { navigation } = this.props
         const quizTitle = 'Results'
         const quizSubTitle = ' Click buttons below to see suggestions for exercises that may help with those issues.'
@@ -58,6 +82,13 @@ export default class SelfCareQuizResultScreen extends Component {
                         })
                     }
                     </View>
+                    <View style={AppStyles.hCenter}>
+                        <CardWithImage
+                            cardImage={PhoneIcon}
+                            text='Talk to Someone'
+                            onPress={() => callPhone(navigation)}
+                        />
+                    </View>
                 </ScrollView>
             </View>
         )
@@ -66,6 +97,35 @@ export default class SelfCareQuizResultScreen extends Component {
 
 const styles = StyleSheet.create({
     cardArea: {
-        flexWrap: 'wrap'
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginHorizontal: Margins.elementM,
+        marginTop: Margins.elementM
+    },
+
+    cardStyle: {
+        width: width / 2 - 30,
+        height: height / 9,
+        margin: Margins.elementM,
+        padding: Paddings.elementP,
+        borderRadius: BorderRadii.boxBR
+    },
+
+    statusText: {
+        color: 'white',
+        fontSize: FontSizes.topicFS,
+        fontWeight: '600'
+    },
+
+    separator: {
+        height: 2,
+        width: width / 2 - 60,
+        backgroundColor: 'white',
+        marginVertical: 5
+    },
+
+    frequencyText: {
+        color: 'white',
+        fontSize: FontSizes.contentFS
     }
 })
