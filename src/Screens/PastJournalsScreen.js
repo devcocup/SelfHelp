23d9efe4b2c10b0e29b71a4fe294a8d3e15e21e8
devac1103 <hdev0301@gmail.com>
2018-04-28 00:47:18 +0800

    Modified PastJournalScreen to display orange pen icon for only fields with history

--- src/Screens/PastJournalsScreen.js
+++ src/Screens/PastJournalsScreen.js
@@ -20,46 +20,13 @@ const onListClick = (item, index, navigation) => {
     navigate('JournalHistoryScreen', { question: item })
 }
 
-const ListContainer = ({ navigation }) => {
-    const list = JournalPromptQuestions.map((item, index) => {
-        let checked = false
-        const db = SQLite.openDatabase({name: 'journalsDB', createFromLocation: '/data/journalsDB.sqlite'})
-        
-        db.transaction((txn) => {
-            txn.executeSql(`SELECT * FROM journals where journal_question = "${item}"`, [], (tx, res) => {
-                let tempJournals = []
-                for (let i = 0; i < res.rows.length; ++i) {
-                    tempJournals.push(res.rows.item(i))
-                }
-                if (tempJournals.length > 0) {
-                    checked = true
-                }
-            })
-        })
-
-        return (
-            <View key={index}>
-                <JournalTitleListItem
-                    checked={checkedState}
-                    label={item}
-                    onPress={() => onListClick(item, index, navigation)}
-                />
-            </View>
-        )
-    })
-
-    return (
-        <View>{list}</View>
-    )
-}
-
 
 export default class PastJournalsScreen extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-            checkedState: false
+            checkedStates: []
         }
     }
 
@@ -73,9 +40,6 @@ export default class PastJournalsScreen extends Component {
                     navigation={navigation}
                 />
                 <ScrollView>
-                    {/*<ListContainer
-                        navigation={navigation}
-                    />*/}
                     <View>
                     {
                         JournalPromptQuestions.map((item, index) => {
@@ -88,8 +52,10 @@ export default class PastJournalsScreen extends Component {
                                         tempJournals.push(res.rows.item(i))
                                     }
                                     if (tempJournals.length > 0) {
+                                        const tempStates = this.state.checkedStates
+                                        tempStates[index] = true
                                         this.setState({
-                                            checkedState: true
+                                            checkedStates: tempStates
                                         })
                                     }
                                 })
@@ -98,7 +64,7 @@ export default class PastJournalsScreen extends Component {
                             return (
                                 <View key={index}>
                                     <JournalTitleListItem
-                                        checked={this.state.checkedState}
+                                        checked={this.state.checkedStates[index]}
                                         label={item}
                                         onPress={() => onListClick(item, index, navigation)}
                                     />
